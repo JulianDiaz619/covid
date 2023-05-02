890f6309be5f120443ec6112b8d88612c06821ee
KP <117617878+khp023@users.noreply.github.com>
2023-05-01 20:19:52 -0700

    directory cleanup

--- resources/worksheets.py
+++
@@ -1,4 +0,0 @@
-# Candidate for underlying conditions contributing to COVID-19
-conditions = "https://data.cdc.gov/api/views/hk9y-quqm/rows.csv?accessType=DOWNLOAD"
-
-surveillance = "https://data.cdc.gov/api/views/n8mc-b4w4/rows.csv?accessType=DOWNLOAD"
